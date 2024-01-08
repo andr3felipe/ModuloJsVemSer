00bd3701960deaac4456d86ca43a7c504c9efc1d
andr3felipe <andre.felipe_680@hotmail.com>
2024-01-08 20:53:04 -0300

    exercise 05

---
+++ Task-01/5.js
@@ -0,0 +1,31 @@
+const prompt = require("prompt-sync")({ sigint: true });	
+
+let start = true;
+
+while(start) {
+  const firstNaturalNumber = getNumber("Insira o primeiro número natural: \n");
+  const secondNaturalNumber = getNumber("Insira o segundo número natural: \n");
+
+  console.log(sum(firstNaturalNumber, secondNaturalNumber));
+
+  start = prompt("\nDeseja fazer uma nova comparação? (S/N): ").toUpperCase() === "S" ? true : false;
+}
+
+
+function getNumber(message) {
+  let input = prompt(message);
+
+  while(parseInt(input) !== Number(input)) {
+    console.log("---> Por favor, insira um número natural válido <---.\n");
+
+    input = prompt(message);
+  }
+
+  return input
+}
+
+function sum(num1, num2) {
+
+  return `O resultado da soma dos números naturais é: ${Number(num1) + Number(num2)}`
+}
+
