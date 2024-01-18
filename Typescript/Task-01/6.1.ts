c348b344261b17eecd43125261f36411a5578771
andr3felipe <andre.felipe_680@hotmail.com>
2024-01-18 17:59:56 -0300

    task-01-ts

---
+++ Typescript/Task-01/6.1.ts
@@ -0,0 +1,9 @@
+function fazPegadinha() {
+  for (let i = 0; i < 9; i++) {
+    alert(
+      `Você foi enganado(a)!\n Pressione ENTER ou clique em OK para fechar esta janela e continuar a navegar no site. \n ${
+        i + 1
+      }`
+    );
+  }
+}
